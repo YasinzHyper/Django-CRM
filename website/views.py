d4a1c3094b334b9a906ad7880e88238cda795552
Raj Kelawala <rajkelawala28@gmail.com>
2025-02-25 20:21:08 +0530

    Add login functionality with success/error messages in home view

--- website/views.py
+++ website/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home(request):
-    return render(request,'home.html', {})
+    #check to see if user is login
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']   
+        #Authenticate 
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request, ('You have been logged in'))
+            return redirect('home')
+        else:
+            messages.success(request, ('There was an error in Login...'))
+            return redirect('home')
+    else:
+        return render(request,'home.html', {})
 
 def login_user(request):
     pass
