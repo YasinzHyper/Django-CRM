089dbd355f0745962f70442f5cace56051aacfdb
Raj Kelawala <rajkelawala28@gmail.com>
2025-02-27 13:46:49 +0530

    Updated

--- website/views.py
+++ website/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
     #check to see if user is login
     if request.method == 'POST':
         username = request.POST['username']
@@ -18,7 +20,7 @@ def home(request):
             messages.success(request, ('There was an error in Login...'))
             return redirect('home')
     else:
-        return render(request,'home.html', {})
+        return render(request,'home.html', {"records": records})
 
 def logout_user(request):
     logout(request)
@@ -32,7 +34,7 @@ def register_user(request):
 			form.save()
 			# Authenticate and login
 			username = form.cleaned_data['username']
-			password = form.cleaned_data['password1']
+			password = form.cleaned_data['password1']   
 			user = authenticate(username=username, password=password)
 			login(request, user)
 			messages.success(request, "You Have Successfully Registered! Welcome!")
@@ -42,3 +44,23 @@ def register_user(request):
 		return render(request, 'register.html', {'form':form})
 
 	return render(request, 'register.html', {'form':form})
+
+def customer_record(request, pk):
+	if request.user.is_authenticated:
+		# Look Up Records
+		customer_record = Record.objects.get(id=pk)
+		return render(request, 'record.html', {'customer_record':customer_record})
+	else:
+		messages.success(request, "You Must Be Logged In To View That Page...")
+		return redirect('home')
+	
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        # Look Up Records
+        customer_record = Record.objects.get(id=pk)
+        customer_record.delete()
+        messages.success(request, "Record Deleted...")
+        return redirect('home')
+    else:
+        messages.success(request, "You Must Be Logged In To Delete That...")
+        return redirect('home')
